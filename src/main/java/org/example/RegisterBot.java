cab21d725cc55e387c670cfc0f50159c32ae17f0
Ana Beatriz <129392016+beatrizlopes30@users.noreply.github.com>
2025-10-06 16:32:34 -0300

    Update RegisterBot.java
    .

--- src/main/java/org/example/RegisterBot.java
+++ src/main/java/org/example/RegisterBot.java
@@ -1,6 +1,6 @@
 package org.example;
 
 public class RegisterBot {
-    public static final String BOT_TOKEN="7661931047:AAFBnKgpSQKMhoM7ofKOvsWsih7J74uh7bI";
+    public static final String BOT_TOKEN="7661931047:AAFBfgpSQKMhoM7ofKOvsWsih7J74uh7bI";
     public static final String BOT_USER_NAME="control_expensebot";
 }
